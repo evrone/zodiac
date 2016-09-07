364b8022fee18955b171ed0e83e38fd058125796
Neodelf <neodelf@gmail.com>
2016-09-07 13:26:39 +0300

    Refactoring activerecord.rb

--- lib/zodiac/activerecord.rb
+++ lib/zodiac/activerecord.rb
@@ -1,34 +1,35 @@
 module Zodiac
   module ActiveRecord
     module InstanceMethods
-      def zodiac_sign
-        raise 'You should call #zodiac_reader in your class for this to work' unless self.class.respond_to?(:date_for_zodiac)
-        send(self.class.date_for_zodiac).try(:zodiac_sign)
+      def zodiac_sign(method=:zodiac_sign)
+        unless self.class.respond_to?(:date_for_zodiac)
+          raise 'You should call #zodiac_reader in your class for this to work'
+        end
+        public_send(self.class.date_for_zodiac).try(method)
       end
 
       Zodiac.each_sign do |symbol, _integer|
         method_name = "#{symbol}?"
-        define_method(method_name) do
-          raise 'You should call #zodiac_reader in your class for this to work' unless self.class.respond_to?(:date_for_zodiac)
-          send(self.class.date_for_zodiac).try(method_name)
-        end
+        define_method(method_name) do                          # def libra?
+          zodiac_sign(method_name)                             #   zodiac_sign(:libra)
+        end                                                    # end
       end
 
     private
       def update_sign_id
         sign_id_method  = "#{self.class.zodiac_sign_id_field}="
-        new_sign_id     = send(self.class.date_for_zodiac).try(:zodiac_sign_id)
-        send(sign_id_method, new_sign_id)
+        new_sign_id     = public_send(self.class.date_for_zodiac).try(:zodiac_sign_id)
+        public_send(sign_id_method, new_sign_id)
       end
     end
 
     module ClassMethods
       attr_reader :date_for_zodiac, :zodiac_sign_id_field
-      
+
       def zodiac_reader(dob_attribute, options = { sign_id_attribute: :zodiac_sign_id })
         @date_for_zodiac = dob_attribute
         @zodiac_sign_id_field = options[:sign_id_attribute]
-        
+
         # if the migration was applied, we should update the sign attribute before each save
         # and define some scopes
         if connection.table_exists?(table_name) && column_names.include?(@zodiac_sign_id_field.to_s)
@@ -47,7 +48,7 @@ module Zodiac
               raise ArgumentError, "Invalid attribute type #{sign.class} for #{self}#by_zodiac"
             end
           end
-          
+
           # Person.gemini == Person.by_zodiac(3)
           Zodiac.each_sign do |symbol, integer|
             scope symbol, -> { by_zodiac(integer) }
@@ -57,12 +58,12 @@ module Zodiac
     end
     
     def self.included(base)
-      base.send :include, InstanceMethods
+      base.include InstanceMethods
       base.extend ClassMethods
     end
   end
 end
 
 if defined?(::ActiveRecord)
-  ActiveRecord::Base.send(:include, Zodiac::ActiveRecord)
+  ActiveRecord::Base.include Zodiac::ActiveRecord
 end
